0b11572d1c6a1265bd5012e48f66d2545f2e5e83
nnesterova <ParolGit1994>
2021-03-21 17:44:41 +0300

    added my files

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.*;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void successfulFillTest() {
+
+       open("https://github.com/selenide/selenide");
+        $(byText("Wiki")).click();
+        $("#wiki-wrapper").shouldHave(text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
